f7c4c4185586d2f7bb7a96a307e87e1755f34927
bpht <bennyphtam@gmail.com>
2022-12-28 15:58:13 +0800

    Impl. js for re-usable nav-bar

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+let homeLink = "#";
+let aboutLink = "#";
+
+let homeItem = `<li><a href=${homeLink}>Home</a><li>`
+let aboutItem = `<li><a href=${aboutLink}>About</a><li>`
+
+let navContent = `<ul>${homeItem}${aboutItem}</ul>`
+
+document.getElementById("nav").innerHTML = navContent;
